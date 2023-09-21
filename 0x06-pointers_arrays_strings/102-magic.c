1177a7f7acf930fd51f191e58a0cedfc2ea009d9
Salmarh1 <salmarhadi5@gmail.com>
2023-09-21 14:38:18 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/*
+ *description: is the main function for the 102-magic.c program.
+ * It demonstrates a coding challenge where we need to modify
+ * the value of a specific array element indirectly.
+ *
+ * The program initializes an array 'a' and an integer 'n'.
+ * It sets a[2] to 1024 and uses a pointer 'p' to modify
+ * a[2] to be 98 without directly referencing 'a'.
+ *
+ * After the modification, it prints the value of a[2].
+ */
+int main(void)
 {
 	int n;
 	int a[5];
@@ -17,6 +29,6 @@
    */
 	*(p + 5) = 98;
   /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
