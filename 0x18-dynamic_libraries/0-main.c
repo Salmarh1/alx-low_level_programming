a89aea6f713d35e71e0a4fb36bab43e3f340e35b
Salmarh1 <salmarhadi5@gmail.com>
2023-12-11 16:51:09 -0800

    dynamic_libraries

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
  *
- * Return: Always (EXIT_SUCCESS).
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
