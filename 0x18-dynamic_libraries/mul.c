800526b7ee7cc57d5010337ddd918c033e8986e3
Salmarh1 <salmarhadi5@gmail.com>
2024-04-29 07:39:49 -0700

    at last

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiplay two integers
+ * @a: first unt
+ * @b: second int
+ * Return: multiplay
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
